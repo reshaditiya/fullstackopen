2c99feea1ba340191369822c11d732ffabecb93e
reshaditiya <reshaditiya@gmail.com>
2023-07-19 19:28:07 +0700

    anecdotes

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react"
+
+const App = () => {
+	const anecdotes = [
+		"If it hurts, do it more often.",
+		"Adding manpower to a late software project makes it later!",
+		"The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+		"Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+		"Premature optimization is the root of all evil.",
+		"Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+		"Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+		"The only way to go fast, is to go well.",
+	]
+
+	const randNum = () => Math.floor(Math.random() * anecdotes.length)
+	const [selected, setSelected] = useState(randNum)
+	const [point, setPoint] = useState({})
+
+	function handleVote(index) {
+		return setPoint((prev) => ({
+			...prev,
+			[index]: (prev[index] || 0) + 1,
+		}))
+	}
+
+	function getMaxPointIndex() {
+		let maxIndex = 0
+		let maxPoint = point[0]
+
+		for (const index in point) {
+			if (point[index] > maxPoint) {
+				maxPoint = Math.max(point[index], maxPoint)
+				maxIndex = index
+			}
+		}
+
+		return maxIndex
+	}
+
+	return (
+		<main>
+			<h1>Anecdote of the day</h1>
+			<p>{anecdotes[selected]}</p>
+			<p>has {point[selected] || 0} votes</p>
+			<button onClick={() => handleVote(selected)}>VOTE</button>
+			<button onClick={() => setSelected(randNum)}>NEXT ANECDOTE</button>
+
+			<h2>Anecdote with most votest</h2>
+			<p>{anecdotes[getMaxPointIndex()]}</p>
+			<p>has {point[getMaxPointIndex()] || 0} votes</p>
+		</main>
+	)
+}
+
+export default App
