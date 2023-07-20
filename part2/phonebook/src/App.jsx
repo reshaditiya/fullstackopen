2055c618d1aa54cfbd123f57d7bbf81c6a4ec0b5
reshaditiya <reshaditiya@gmail.com>
2023-07-20 21:19:06 +0700

    Exercise 2.11.

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { SearchField } from "./components/SearchField"
 import { DataForm } from "./components/DataForm"
 import { DataDisplay } from "./components/DataDisplay"
+import axios from "axios"
 
 const App = () => {
-	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "040123456" },
-		{ name: "Ada Lovelace", number: "39445323523" },
-		{ name: "Dan Abramov", number: "1243234345" },
-		{ name: "Mary Poppendieck", number: "39236423122" },
-	])
+	const [persons, setPersons] = useState([])
 	const [newPerson, setNewPerson] = useState({ name: "", number: "" })
 	const [keyword, setKeyword] = useState("")
 
@@ -27,6 +23,22 @@ const App = () => {
 	function isNameExist(number) {
 		return persons.find((person) => person.number === number)
 	}
+
+	useEffect(() => {
+		async function getPersons() {
+			try {
+				const persons = (
+					await axios.get("http://localhost:3001/persons")
+				).data
+
+				setPersons(persons)
+			} catch (error) {
+				console.log("Error getting person data: ", error)
+			}
+		}
+
+		getPersons()
+	}, [])
 
 	return (
 		<div>
